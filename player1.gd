87cb0f240d697b231229942a9bf72245c06c367c
Samantha <samantha.patterson@gmail.com>
2023-04-06 19:58:01 -0700

    Added minimal zapper and several TODOs

--- player1.gd
+++ player1.gd
@@ -1,12 +1,33 @@
 extends CharacterBody2D
 
-const SPEED := 40.0
+const WALK_SPEED := 40.0
+const ZAP_SPEED := 60.0
+const ZAP_RANGE := 600.0
+const ZAP_RECHARGE_TIME := 0.5  #seconds
+
+@onready var zap := $Hook
+@onready var zap_shape := $Hook/CollisionShape2D
+
+#For now limit zaps to 1 zap at a time and have a recharge time for them
+enum ZapStates {NONE, ZAPPING, RECHARGE}
+
+#TODO: Placeholder for different morphs having different zaps.
+enum ZapType {BEAM, LOB}
 
 var player_num := 1
 var action_move_right := "player1_right"
 var action_move_left := "player1_left"
 var action_move_down := "player1_down"
 var action_move_up := "player1_up"
+var action_zap  := "player1_zap"
+var input_direction := Vector2(0,0)      #Last direction of movement detected.
+var zap_direction := Vector2(0,0)
+var zap_state: ZapStates = ZapStates.NONE
+var zap_recharge_timer = Timer.new()
+
+func _on_zap_recharge():
+	self.zap_state = ZapStates.NONE
+	zap_hide()   #TODO probably don't need this here.
 
 # func _init():  //SET ONLY DON'T READ use _ready()
 
@@ -21,21 +42,65 @@ func _ready():
 		self.action_move_left = prefix + "_left"
 		self.action_move_up = prefix + "_up"
 		self.action_move_down = prefix + "_down"
+		self.action_zap = prefix + "_zap"
+		add_child(self.zap_recharge_timer)
+		#zap_recharge_timer.connect("timeout",self,"_on_zap_recharge")
+		self.zap_recharge_timer.wait_time = ZAP_RECHARGE_TIME
+		self.zap_recharge_timer.one_shot = false
+
+
 		
 		print("Settings set to " + self.action_move_right + " " + self.action_move_left)
 		
 
+func zap_hide():
+	zap.hide()
+	zap_shape.set_disabled.call_deferred(true) 
+	
 
 func get_input():
-	var input_direction = Input.get_vector(action_move_left, 
+	self.input_direction = Input.get_vector(action_move_left, 
 											action_move_right, 
 											action_move_up,
 											action_move_down)
-	velocity = input_direction * SPEED
+	velocity = self.input_direction * WALK_SPEED
 
+func do_zap():
+	#Fire the zappy beam. 
+	if self.input_direction == Vector2(0,0):	#No direction to zappy.
+		return
+	
+	match self.zap_state:
+		ZapStates.NONE:		#Zap the zapper
+			if Input.is_action_just_pressed(self.action_zap):
+				self.zap_state = ZapStates.ZAPPING
+				zap.global_position = global_position
+				self.zap_direction = self.input_direction
+				zap.show()
+				zap_shape.disabled = false
+		ZapStates.RECHARGE:
+			#Wait for cooldown.  This is handled in a callback.
+			#TODO: Could add a sound to indicate waiting. *CLICK*
+			pass 
+		#ZapStates.ZAPPING:  We don't want an edge case later where we just 
+		#changed from none so let's handle this outside the match.
+			
+	#State could have changed from NONE to ZAPPING so check here.
+	if self.zap_state == ZapStates.ZAPPING:  
+		zap.global_position += zap_direction * ZAP_SPEED
+		if zap.global_position.distance_to(global_position) >= ZAP_RANGE:
+			#TODO FIX TIMER zap_state = ZapStates.RECHARGE
+			zap_state = ZapStates.NONE
+			self.zap_recharge_timer.start()
+			zap_hide()
+			
+
+	
 func _physics_process(delta: float) -> void:
 	get_input()
+	do_zap()
 	move_and_slide()
+
 
 @onready var _animated_sprite = $AnimatedSprite2D
 
